06eb2253af2fe754885e820bac5707ab7eab1d56
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2024-12-12 09:44:16 -0300

    cria repo

--- pages/api/models/Metas.js
+++ pages/api/models/Metas.js
@@ -1,16 +1,19 @@
 import mongoose from "mongoose";
 
 const MetasSchema = new mongoose.Schema({
+  userId: { type: String, required: true }, // Relaciona o produto ao cliente
+  intervalo: { type: String, required: true }, // se e  intervalo que vai cumprir e mensal semanal diario
+  prazo: { type: String, required: true },  // o prazo de quanto tempo vai dura uma meta 3 meses, um ano etc...
+  horas: { type: String, required: true },  // quantas hora por dia na meta
+  categoria: { type: String, required: true }, // Campo tipo corretamente definido
   nome: { type: String, required: true },
-  preco: { type: Number, required: true },
+  valor: { type: Number, required: true },
   statusDaMeta: {
     type: String,
     enum: ["pendente", "concluida", "expirada"],
     default: "pendente",
   },
   dataCriacao: { type: Date, default: Date.now },
-  tipo: { type: String, required: true }, // Campo tipo corretamente definido
-  userId: { type: String, required: true }, // Relaciona o produto ao cliente
 });
 
 export default mongoose.models.Metas || mongoose.model("Metas", MetasSchema);
