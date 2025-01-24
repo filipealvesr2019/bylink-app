2640a8b740d244c5617b362a3a3f39118bc07714
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2025-01-24 17:21:25 -0300

    refactor: refatora estilos

--- pages/api/routes/temas.js
+++ pages/api/routes/temas.js
@@ -30,6 +30,7 @@ export default async function handler(req, res) {
 
       // Criar o novo produto
       const newProduct = new Links({
+        userId,
         nome,
         backgroundColor, 
         linksColor, 
