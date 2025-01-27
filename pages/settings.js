3995af583d93789f332871f7316c04b81d400b68
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2025-01-27 12:11:16 -0300

    feat: adiciona campos da pagina de links

--- pages/settings.js
+++ pages/settings.js
@@ -1,10 +1,44 @@
-import Layout from "/components/Layout";
+import axios from "axios";
+
+import { useEffect, useState } from "react";
+import { useUser } from "@clerk/nextjs";
+
+import Login from "@/app/Login";
+import MobileMenu from "../components/MobileMenu/MobileMenu";
 
 export default function Settings() {
+  const { user } = useUser();
+
+  const [data, setData] = useState([]);
+  const getTemas = async () => {
+    try {
+      const response = await axios.get(
+        `http://localhost:3000/api/routes/temas`
+      );
+      setData(response.data.links);
+      console.log("setName", name);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getTemas();
+  }, []);
   return (
-    <Layout>
-      <h1>Configurações</h1>
-      <p>Configure as opções do seu LinkFree.</p>
-    </Layout>
+    <div
+      style={{
+        backgroundColor: "#fff",
+        height: "100vh",
+      }}
+    >
+      <MobileMenu />
+      <Login />
+
+
+        {data.map((link) => (
+          <>{link.name}</>
+        ))}
+    
+    </div>
   );
 }
