fba698984695790da91c5953e9b85e14e086d2ea
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2025-03-12 11:07:54 -0300

    feat: adiciona pagamentos de assinatura

--- pages/payment.js
+++ pages/payment.js
@@ -3,16 +3,14 @@ import MobileMenu from "../components/MobileMenu/MobileMenu";
 import styles from "./payment.module.css";
 export default function payment() {
   return (
-    <>
-     
-      <div className={styles.container}>
-        <div>
-            teste
+    <div className={styles.container}>
+      <div className={styles.menu}>
+        <div className={styles.container__a}>teste</div>
+        <div className={styles.container__b}>
+          <h1>Valor Total </h1>
+          <button className={styles.button}>Atualizar Assinatura</button>
         </div>
-        <div>
-            <h1>Valor Total </h1>
-            </div>
       </div>
-    </>
+    </div>
   );
 }
