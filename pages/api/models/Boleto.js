4dbc5d699dbb2d3250eb9fda7137db955c859f25
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2025-03-16 10:32:16 -0300

    feat: adiciona boleto como forma de pagamento

--- pages/api/models/Boleto.js
+++ pages/api/models/Boleto.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const Boletoschema = new mongoose.Schema({
   userId: { type: String, required: true }, // Relaciona o produto ao cliente
   subscriptionId:  { type: String}, // Relaciona o produto ao cliente
-  bankSlipUrl: { type: String,},
+  bankSlipUrl: { type: String},
   dataCriacao: { type: Date, default: Date.now },
 });
 
--- pages/api/routes/boleto-semiannually-subscription-plain-pro.js
+++ pages/api/routes/boleto-semiannually-subscription-plain-pro.js
@@ -14,19 +14,19 @@ export default async function handler(req, res) {
   }
 
   try {
-   
-
     // Conecta ao banco de dados
     await dbConnect();
 
     const customer = await Clientes.findOne({ userId });
     const asaasId = customer?.asaasId;
     const subscriptions = await Subscriptions.findOne({ userId });
-    const id = subscriptions.subscriptionId
+    const id = subscriptions.subscriptionId;
     // const {id} = req.body;
- // Verifica se já existe uma assinatura para esse usuário
+    // Verifica se já existe uma assinatura para esse usuário
     if (!userId || !asaasId) {
-      return res.status(400).json({ message: "userId e customerId são obrigatórios" });
+      return res
+        .status(400)
+        .json({ message: "userId e customerId são obrigatórios" });
     }
 
     const url = `https://api-sandbox.asaas.com/v3/subscriptions/${id}/payments?status=PENDING`;
@@ -36,26 +36,39 @@ export default async function handler(req, res) {
         accept: "application/json",
         access_token: token, // Pegando o token da env
       },
-   
     };
 
     const response = await fetch(url, options);
-    console.log(response)
+    // console.log(response)
     // Verifica se há co nteúdo antes de chamar .json()
     const text = await response.text();
     const data = text ? JSON.parse(text) : null;
 
     if (!response.ok) {
       console.error("Erro da API Asaas:", data || "Erro desconhecido");
-      return res.status(response.status).json({ error: data || "Erro desconhecido da API Asaas" });
+      return res
+        .status(response.status)
+        .json({ error: data || "Erro desconhecido da API Asaas" });
     }
 
-    const novoBoleto = new Boleto({ userId: userId, bankSlipUrl: data.bankSlipUrl});
-    await novoBoleto.save();
+    // Verifique se o URL do boleto está presente na resposta
+    if (data && data.data && data.data[0] && data.data[0].bankSlipUrl) {
+      const novoBoleto = new Boleto({
+        userId: userId,
+        bankSlipUrl: data.data[0].bankSlipUrl,
+        subscriptionId: id,
+      });
+      await novoBoleto.save();
+    }
+
     //  console.log('boleto-monthly-subscription-plain-pro', data)
-    return res.status(201).json({ message: "Assinatura criada com sucesso", data });
+    return res
+      .status(201)
+      .json({ message: "Assinatura criada com sucesso", data });
   } catch (error) {
     console.error("Erro ao processar a requisição:", error);
-    return res.status(500).json({ error: error.message || "Erro interno do servidor" });
+    return res
+      .status(500)
+      .json({ error: error.message || "Erro interno do servidor" });
   }
 }
