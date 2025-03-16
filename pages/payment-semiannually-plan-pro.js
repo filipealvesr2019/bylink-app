f0824481d7126fd81c365b915b0d0b0fe8bd02f9
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2025-03-16 10:01:11 -0300

    feat: adiciona boleto como forma de pagamento

--- pages/payment-semiannually-plan-pro.js
+++ pages/payment-semiannually-plan-pro.js
@@ -46,6 +46,40 @@ export default function Payment() {
     }
   };
 
+
+  
+  const handleBoletoPayment = async () => {
+    setLoading(true);
+    try {
+      const response = await axios.get("/api/routes/boleto-semiannually-subscription-plain-pro");
+      if (response.status === 201) {
+      // Redirecionar para a URL de pagamento PIX
+      window.location.href = response.data.data.data[0].bankSlipUrl;
+      }
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      console.error("Erro ao gerar o QR Code:", error.response?.data || error.message);
+    }
+  };
+  
+  const handleMonthlySubscriptionBoleto = async () => {
+    setLoading(true);
+    try {
+      const response = await axios.post("/api/routes/semiannually-subscription-boleto-plain-pro");
+      if (response.status === 201) {
+           
+      handleBoletoPayment()
+      }
+    
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      console.error("Erro ao realizar a assinatura:", error.response?.data || error.message);
+    }
+  };
+
+
   // UseEffect para redirecionar após a atualização do estado encodedImage
   useEffect(() => {
     if (encodedImage) {
@@ -61,6 +95,9 @@ export default function Payment() {
       <div className={styles.menu}>
         <div className={styles.container__a}>
           <button onClick={handlePixPayment}>Pagar com Pix</button>
+        </div>
+        <div className={styles.container__a}>
+          <button onClick={handleMonthlySubscriptionBoleto}>Pagar com Boleto</button>
         </div>
         <div className={styles.container__b}>
           <h1>Valor Total</h1>
